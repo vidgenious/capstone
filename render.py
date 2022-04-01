128525c744e0b223c26050b8688fe16879bfcdda
braedenlane <braedenlane10@gmail.com>
2022-03-31 18:44:38 -0700

    Can screenshot a fed in stl

--- render.py
+++ render.py
@@ -3,12 +3,18 @@ from stl.mesh import Mesh
 
 # TODO: Slice by layer
 def meshFullSTL():
-    path = vpl.data.get_rabbit_stl()
+    # path = vpl.data.get_rabbit_stl()
+    path = 'Gengar_-_mesh_fixed.stl'
 
     mesh = Mesh.from_file(path)
 
     vpl.mesh_plot(mesh)
-    vpl.show()
+    vpl.view(camera_direction=[-1, 1, 1])
+    vpl.reset_camera()
+    #vpl.show()
+    vpl.save_fig("image.jpg", pixels=(600,600), off_screen=True)
+
+    vpl.close()
 
 if __name__ == '__main__':
     meshFullSTL()
