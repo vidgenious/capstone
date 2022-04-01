4d58d093b3667a13941af56f9ce9c24da7def5b5
vidgenious <63082621+vidgenious@users.noreply.github.com>
2022-04-01 13:33:25 -0700

    solid code

--- render.py
+++ render.py
@@ -3,6 +3,35 @@ from stl.mesh import Mesh
 
 # TODO: Slice by layer
 def meshFullSTL():
+    # a = import_stl("Gengar_-_mesh_fixed.stl")
+
+    # # scad_render_to_file(a, "temp.scad")
+
+    # # a = import_scad("temp.scad")
+
+    # rotate
+
+    # d = difference()(
+    #     a,
+    #     up(30)(back(50)(left(100)(cube(200))))
+    # )    
+    # print(type(cube(20)))
+    # print(type(a))
+
+    # rotate_extrude(180, 180, 180)(
+    #     d
+    # )
+
+    # scad_render_to_file(d, "temp.scad")
+
+    # # os.system("openscad {} -o {}".format("temp.scad", "result.stl"))
+    # print("here")
+    # # os.system("openscad -o result.stl temp.scad")
+    # os.system("openscad --preview --imgsize=512,512 {} -o {}".format("temp.scad", "out.png"))
+
+    # print("here")
+
+
     # path = vpl.data.get_rabbit_stl()
     path = 'Gengar_-_mesh_fixed.stl'
 
