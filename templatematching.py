1f0b0d3891b0aad71dc2f7d73787f60c45131bf0
braedenlane <braedenlane10@gmail.com>
2022-04-03 23:38:31 -0700

    Successful image processing of images, dummy pause and resume calls to be added to the Raspberry Pi

--- templatematching.py
+++ templatematching.py
@@ -1,28 +1,22 @@
-import cv2
+import cv2 as cv
 import numpy as np
 
-THRESHOLD = 0.9
+THRESHOLD = 0.7
 
-# Image path and template path here TODO: Fill paths
-image = cv2.imread("")
-template = cv2.imread("")
+image = cv.imread("fullimage.jpg")
+template = cv.imread("template2.jpg")
+imageGray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+templateGray = cv.cvtColor(template, cv.COLOR_BGR2GRAY)
 
 # This results in a plot that compares the similarity
-matchPlot = cv2.matchTemplate(image, template, cv2.TM_CCOEFF_NORMED)
+matchPlot = cv.matchTemplate(imageGray, templateGray, cv.TM_CCOEFF_NORMED)
 
-# Compare it to make sure only detections occur where there is a 90% match
-match_locations = np.where(matchPlot >= THRESHOLD)
+(minVal, maxVal, minLoc, maxLoc) = cv.minMaxLoc(matchPlot)
 
-# Populate detections that match
-detections = []
-for(x, y) in (match_locations[1], match_locations[0]):
-    match = {
-        "MATCH_VALUE": matchPlot[y, x],
-    }
+if maxVal < THRESHOLD:
 
-    detections.append(match)
-
-    # Check if no detections for match
-    if not bool(detections):
-        # run the pause command, error occurred TODO: Pause command
-        print("Pause")
+    # run pause command
+    print("Pause")
+else:
+    # continue
+    print("Continue")
